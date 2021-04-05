95f541e9a1f4c7f9d9afac3699238759706dd35e
darshansp8 <d.sakpal38@gmail.com>
2021-04-05 22:20:55 +0530

    Added other components and connected register form with the db

--- Complaint-System/src/app/app.module.ts
+++ Complaint-System/src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { UserTicketComponent } from './user-ticket/user-ticket.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
---
+++ Complaint-System/src/app/user-ticket/user-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-ticket',
+  templateUrl: './user-ticket.component.html',
+  styleUrls: ['./user-ticket.component.css']
+})
+export class UserTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
