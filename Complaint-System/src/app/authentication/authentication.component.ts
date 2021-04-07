fc894e25ec265f401e05248ebb5b3aa21168e718
darshansp8 <d.sakpal38@gmail.com>
2021-04-08 00:15:32 +0530

    Tried routing of login and register

--- Complaint-System/src/app/authentication/authentication.component.ts
+++ Complaint-System/src/app/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+  myComponent: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log(this.route.snapshot);
+
   }
 
 }
